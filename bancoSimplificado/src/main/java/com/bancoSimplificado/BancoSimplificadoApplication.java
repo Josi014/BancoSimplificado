620b1fda7da1769ae70afabc2e49249563194981
Joseli Ramos <josielisouzaderamos714@gmail.com>
2024-08-02 00:10:29 -0300

    feat: first commit

---
+++ bancoSimplificado/src/main/java/com/bancoSimplificado/BancoSimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.bancoSimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoSimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoSimplificadoApplication.class, args);
+	}
+
+}
---
+++ bancoSimplificado/src/test/java/com/bancoSimplificado/BancoSimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancoSimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoSimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
